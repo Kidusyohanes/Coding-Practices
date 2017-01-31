28363820d702fda29a9875a25e4c9ae0e0fdd593
Kidusyohanes <kidussendeke@gmail.com>
2017-01-31 10:37:08 -0800

    Update AlgorithmPractices.java

--- AlgorithmPractices.java
+++ AlgorithmPractices.java
@@ -24,4 +24,31 @@ public class AlgorithmPractices {
 			}
 		}
 	}
+	
+	// It takes in a list of integers and calculates a range of that list. 
+	public static int range(ArrayList<Integer> list){
+		if(list.size() > 0){
+			int min = list.get(0); 
+	        int max = list.get(0); 
+	        for(int i = 0; i < list.size(); i++){
+	           min = Math.min(min,list.get(i));
+	           max = Math.max(max,list.get(i));
+	        }
+	        return max-min + 1; 
+	     } else {
+	        return 0; 
+	     }
+	}
+	
+	// This method takes a row and column & prints a grid of numbers. 
+	public static void printGrid(int row, int col){
+	     for(int i = 1; i <= row; i++){
+	        System.out.print(i);
+	        for(int j = row + 1; j <= col; j++){
+	           System.out.print(", " + j);
+	        }
+	        System.out.println();
+	     }
+    }
 }
+
